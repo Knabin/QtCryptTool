28595f0e19d78381cf6870ab2014cd52d5f1f0ef
Nabin Kim <knabinn97@gmail.com>
2021-09-30 19:03:54 +0900

    flow 구성 (TODO)

--- QtCryptTool.cpp
+++ QtCryptTool.cpp
@@ -10,4 +10,14 @@ QtCryptTool::~QtCryptTool()
 
 void QtCryptTool::Start(const crypt_info& cryptInfo)
 {
+	// 1. Path 확인하는 함수
+	
+	// 2. cryptor 세팅한다
+
+	// 3. 정보 출력해 주는 함수 (파일 읽어와서 path, key값, overwrite 여부, 원래 내용, 변환 내용)
+
+	// 4. 저장하는 함수
+	// 이때 overwrite 확인하고 파일명 지정하고..
+
+	// TODO : exit code 정의
 }
